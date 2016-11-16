08e0f311ec8f1b70041f683bf67d21a368e883e2
Ottokar Tilk <ottokart@gmail.com>
2016-11-16 09:14:40 +0200

    Europarl example

---
+++ example/README.md
@@ -0,0 +1,10 @@
+This example produces the preprocessed Europarl English corpus that can be then used for training a model.
+
+Requires nltk
+
+Usage example:
+`./run.sh`
+`cd ..`
+`python data.py ./example/out`
+`python main.py ep 256 0.02`
+`python play_with_model.py Model_ep_h256_lr0.02.pcl`
---
+++ example/dont_run_me_run_the_other_script_instead.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+
+from __future__ import division
+from nltk.tokenize import word_tokenize
+
+import os
+import codecs
+import re
+import sys
+
+NUM = '<NUM>'
+
+EOS_PUNCTS = {".": ".PERIOD", "?": "?QUESTIONMARK", "!": "!EXCLAMATIONMARK"}
+INS_PUNCTS = {",": ",COMMA", ";": ";SEMICOLON", ":": ":COLON", "-": "-DASH"}
+
+forbidden_symbols = re.compile(r"[\[\]\(\)\/\\\>\<\=\+\_\*]")
+numbers = re.compile(r"\d")
+multiple_punct = re.compile(r'([\.\?\!\,\:\;\-])(?:[\.\?\!\,\:\;\-]){1,}')
+
+is_number = lambda x: len(numbers.sub("", x)) / len(x) < 0.6
+
+def untokenize(line):
+    return line.replace(" '", "'").replace(" n't", "n't").replace("can not", "cannot")
+
+def skip(line):
+
+    last_symbol = line[-1]
+    if not last_symbol in EOS_PUNCTS:
+        return True
+
+    if forbidden_symbols.search(line) is not None:
+        return True
+
+    return False
+
+def process_line(line):
+
+    tokens = word_tokenize(line)
+    output_tokens = []
+
+    for token in tokens:
+
+        if token in INS_PUNCTS:
+            output_tokens.append(INS_PUNCTS[token])
+        elif token in EOS_PUNCTS:
+            output_tokens.append(EOS_PUNCTS[token])
+        elif is_number(token):
+            output_tokens.append(NUM)
+        else:
+            output_tokens.append(token.lower())
+
+    return untokenize(" ".join(output_tokens) + " ")
+
+skipped = 0
+
+with codecs.open(sys.argv[2], 'w', 'utf-8') as out_txt:
+    with codecs.open(sys.argv[1], 'r', 'utf-8') as text:
+
+        for line in text:
+
+            line = line.replace("\"", "").strip()
+            line = multiple_punct.sub(r"\g<1>", line)
+
+            if skip(line):
+                skipped += 1
+                continue
+
+            line = process_line(line)
+
+            out_txt.write(line + '\n')
+
+print "Skipped %d lines" % skipped
