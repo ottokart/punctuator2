4058f739d69b091b004d4793df766c8be1508012
Chris Spencer <chrisspen@gmail.com>
2019-09-26 20:11:37 -0400

    Dropped py35 support.

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
